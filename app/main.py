e828bfe21a51e3de36fda752299f2d4e4503a369
jam <dpj0225@gmail.com>
2025-04-16 14:08:10 +0800

    publish 1.0 .

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 from app.api.v1 import external
 from .core.exception_handlers import register_exception_handlers
 
-app = FastAPI(title="ACL CS2 External API", version="0.0.1")
+app = FastAPI(title="ACL CS2 External API", version="1.0")
 app.include_router(external.router)
 register_exception_handlers(app)
